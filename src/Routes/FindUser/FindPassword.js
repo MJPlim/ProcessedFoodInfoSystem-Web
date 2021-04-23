c289a29f8ade7f0e85ffae13d46153fe247d80ee
YiGeon Park <parkegun@gmail.com>
2021-04-23 23:52:43 +0900

    add find password - exception, loading

--- src/Routes/FindUser/FindPassword.js
+++ src/Routes/FindUser/FindPassword.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import "./FindUserStyle.scss"
-import {Alert, Button, Card, CardTitle, Col, Container, Input} from "reactstrap";
+import {Alert, Button, Card, CardTitle, Col, Container, Input, Spinner} from "reactstrap";
 import isEmail from "validator/es/lib/isEmail";
 import axios from "axios";
 import {findPasswordApi} from "../../api";
@@ -9,31 +9,40 @@ const FindPassword = () => {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState(null);
     const [success, setSuccess] = useState(false);
-    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
 
 
     const onChange = (e) => {
         setEmail(e.target.value);
     };
+
+    const makeErrorMessage = (e) => {
+        if (e === 400) {
+            setMessage('가입되지 않은 이메일 입니다.')
+        }
+        setLoading(false);
+    }
+
+
     const emailSubmit = () => {
+        setLoading(true);
         if (!isEmail(email)) {
             setMessage('잘못된 이메일 형식 입니다.')
         } else {
             setMessage(null)
             findPasswordApi.postEmail(email)
                 .then(function (response) {
-                    console.log(response)
+                    setSuccess(true);
+                    setLoading(false);
+                    alert('입력하신 이메일로 임시번호를 발송하였습니다.')
                 })
                 .catch(function (error) {
-                    console.log(error.response)
-                    setError(error.response);
+                    makeErrorMessage(error.response.status);
+                    console.log(error.response.status);
                 })
         }
     };
 
-    // if (error.status == 400) {
-    //     setMessage('가입되지 않은 이메일 입니다.')
-    // }
 
     return (
         <div className="FindUser">
@@ -56,7 +65,10 @@ const FindPassword = () => {
                             : null}
                     </Col>
                     <Col>
-                        <Button onClick={emailSubmit} className="submitButton">확인</Button>
+
+                        {loading ?
+                            <Spinner className="loadingSpinner" color="secondary"/> :
+                            <Button onClick={emailSubmit} className="submitButton">확인</Button>}
                     </Col>
                 </Card>
 
