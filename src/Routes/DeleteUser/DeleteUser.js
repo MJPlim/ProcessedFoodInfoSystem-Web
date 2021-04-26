f621e678900bb9bf7223f1177f54cca19edeba18
jelly-boop <jellyjelly111@naver.com>
2021-04-26 12:34:34 +0900

    form 간단한 ui
    최소한의 ui와 로그인 후 메인페이지로 돌아가도록

--- src/Routes/DeleteUser/DeleteUser.js
+++ src/Routes/DeleteUser/DeleteUser.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import {Alert, Button, Card, CardTitle, Col, Container, Input} from "reactstrap";
 
 function DeleteUser() {
     const [password, setPassword] = useState("");
@@ -16,19 +17,32 @@ function DeleteUser() {
             },
         }).then((response) => {
             console.log(response);
+            alert("탈퇴하셨습니다");
         });
     };
 
     return (
-        <div className="withdrawal">
-            <input
-                type="text"
-                onChange={(e) => {
-                    setPassword(e.target.value);
-                }}
-                placeholder="비밀번호 확인"
-            />
-            <button onClick={withdrawal}>회원탈퇴</button>
+      <div className="FindUser">
+            <Container>
+                <p className="title">탈퇴하기</p>
+                <Card body>
+                    <Col>
+                        <CardTitle className="card-title">비밀번호를 입력해주세요.</CardTitle>
+                    </Col>
+                    <Col md="6">
+                        <div class="form-group">
+                            <label for="exampleDropdownFormPassword1">비밀번호</label>
+                            <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password"
+      />
+    </div>
+                    </Col>
+                    <Col>
+                        <Button onClick={withdrawal} className="submitButton">탈퇴하기</Button>
+                    </Col>
+                </Card>
+
+            </Container>
+
         </div>
     );
 }
--- src/Routes/Login/Login.js
+++ src/Routes/Login/Login.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import {Container,Card} from "reactstrap";
+import  "./LoginStyle.scss";
+import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 var token;
 
 function Login() {
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
-
+    const history = useHistory();
     const login = () => {
         axios({
             url: "http://13.124.55.59:8080/login",
@@ -21,33 +24,54 @@ function Login() {
                 token = response.headers.authorization;
                 console.log(token);
                 localStorage.setItem("authorization", token);
+              
             })
             .catch((error) => {
                 const status = error.response.status;
                 if (status === 401) {
                     console.log("fail");
+                  
                 }
             });
+             history.push('/');//로그인 후 메인페이지로..
     };
 
     return (
-        <div className="login">
-            <h1>로그인</h1>
-            <input
-                type="text"
-                onChange={(e) => {
+     <div className="login">
+            <Container>
+            <p className="title">로그인</p>
+                <Card body>
+              <form >
+    <div class="form-group">
+      <label for="exampleDropdownFormEmail1">이메일</label>
+      <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"
+       onChange={(e) => {
                     setEmail(e.target.value);
                 }}
-                placeholder="이메일을입력하세요"
-            />
-            <input
-                type="password"
-                onChange={(e) => {
+      />
+    </div>
+    <div class="form-group">
+      <label for="exampleDropdownFormPassword1">비밀번호</label>
+      <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Password"
+        onChange={(e) => {
                     setPassword(e.target.value);
                 }}
-                placeholder="비밀번호입력하세요"
-            />
-            <button onClick={login}>로그인</button>
+      />
+    </div>
+     <div class="form-check">
+      <input type="checkbox" class="form-check-input" id="dropdownCheck"/>
+      <label class="form-check-label" for="dropdownCheck">
+        자동 로그인
+      </label>
+    </div>
+     <button onClick={login} type="submit" class="btn btn-danger">로그인</button>
+  </form>
+  <div class="dropdown-divider"></div>
+  <Link class="dropdown-item" to="/join">회원가입하기</Link>
+  <Link class="dropdown-item" to="/findUser/password">비밀번호를 잊으셨나요?</Link>
+</Card>
+            </Container>
+
         </div>
     );
 }
