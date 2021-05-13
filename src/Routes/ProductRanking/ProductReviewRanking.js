e5cd0e11e4796357778956334a5d1773230e896c
YiGeon Park <parkegun@gmail.com>
2021-05-14 03:13:34 +0900

    PLIM-165 add product ranking

--- src/Routes/ProductRanking/ProductReviewRanking.js
+++ src/Routes/ProductRanking/ProductReviewRanking.js
@@ -1,5 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {foodDetailApi, getReviewRankingApi} from "../../api";
+import {Button, Col, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText, Row} from "reactstrap";
+import "./Ranking.scss"
+import {AiFillStar, AiOutlineStar, FaTrophy} from "react-icons/all";
+import {Link} from "react-router-dom";
 
 const ProductReviewRanking = () => {
     const [rankingList, setRankingList] = useState(null);
@@ -38,8 +42,47 @@ const ProductReviewRanking = () => {
 
 
     return (
-        <div>
-            asda
+        <div className="Ranking">
+            <Row className="titleArea">
+                <Col md="7">
+                    <p className="title">제품 랭킹</p>
+                </Col>
+            </Row>
+            <hr className="hr"/>
+            {/* 타이틀 영역 끝 */}
+
+            {!loading && (
+                <ListGroup className={"RankingList"}>
+                    {rankingList.map((item, index) => (
+
+                            <ListGroupItem className={"RankingListItem"}>
+
+
+                                <Link to={{
+                                    pathname: `searchProduct/food/${item.foodId}`
+                                }}>
+
+                                    {index === 0 && (<FaTrophy className={"rankNum first"}/>)}
+                                    {index === 1 && (<FaTrophy className={"rankNum second"}/>)}
+                                    {index === 2 && (<FaTrophy className={"rankNum third"}/>)}
+                                    {index > 2 && (<text className={"rankNum"}>{index + 1}</text>)}
+
+                                    <text className={"foodName"}> {item.foodName}</text>
+                                </Link>
+
+
+                                <text className={"avgRating"}>{item.avgRating}</text>
+                                <AiFillStar className={"ratingStar"}/>
+
+
+                            </ListGroupItem>
+
+                        )
+                    )}
+                </ListGroup>
+            )}
+
+
         </div>
     );
 };
