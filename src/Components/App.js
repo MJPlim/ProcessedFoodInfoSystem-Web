c49d55c36396c6eec4c8c38da2fca30d86a5ca7e
PiaoZhengxun <pjh951218@naver.com>
2021-05-17 10:32:40 +0900

    토큰 유지 방법 보안

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,44 @@
-import React, {Component} from "react";
-import Router from "./Router";
-import GlobalStyle from "./GlobalStyle";
+import React, { Component } from 'react';
+import Router from './Router';
+import GlobalStyle from './GlobalStyle';
+import axios from 'axios';
+
 class App extends Component {
-  
-  render(){
-    
+  componentWillMount() {
+    axios({
+      url: 'http://13.124.55.59:8080/login',
+      method: 'POST',
+      data: {
+        email: localStorage.getItem('userLoginEmail'),
+        password: localStorage.getItem('userLoginPassword'),
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        console.log('새롭게 토큰 받아오기 ------------------');
+        localStorage.setItem('authorization', response.headers.authorization);
+        console.log(localStorage.getItem('authorization'));
+        alert('새로운 토큰 잘 받아왔슴 땡큐 베리 머치');
+      })
+      .catch((error) => {
+        const status = error.response.status;
+        if (status === 401) {
+          //console.log("fail");
+          alert(
+            '입력된 정보가 잘못되었음 여기 app에서 componentWillMount에서 하는 부분이니까 다시 살펴봐주셈.',
+          );
+        }
+      });
+  }
+
+  render() {
     return (
-    <>
-    <GlobalStyle/>
-    <Router/>
-    </>
+      <>
+        <GlobalStyle />
+        <Router />
+      </>
     );
   }
 }
