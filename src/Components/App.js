c3dc9fd0e053b39aa21f749187f952b79e450d4f
PiaoZhengxun <pjh951218@naver.com>
2021-05-27 14:32:55 +0900

    Remove Alert

--- src/Components/App.js
+++ src/Components/App.js
@@ -17,19 +17,13 @@ class App extends Component {
       },
     })
       .then((response) => {
-        console.log('새롭게 토큰 받아오기 ------------------');
+        console.log('Create New Token');
         localStorage.setItem('authorization', response.headers.authorization);
         console.log(localStorage.getItem('authorization'));
-        console.log('새로운 토큰 잘 받아왔슴 땡큐 베리 머치');
       })
       .catch((error) => {
         const status = error.response.status;
-        if (status === 401) {
-          //console.log("fail");
-          alert(
-            '입력된 정보가 잘못되었음 여기 app에서 componentWillMount에서 하는 부분이니까 다시 살펴봐주셈.',
-          );
-        }
+        console.log(status);
       });
   }
 
