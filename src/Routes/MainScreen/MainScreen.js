30af0471072e8da7fad1303e0bdaa7605f124939
PiaoZhengxun <pjh951218@naver.com>
2021-05-27 23:05:56 +0900

    메인

--- src/Routes/MainScreen/MainScreen.js
+++ src/Routes/MainScreen/MainScreen.js
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Spinner} from 'reactstrap';
+import { Spinner } from 'reactstrap';
 import './MainScreenStyle.scss';
+import {
+  Row,
+  Container,
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+  Col,
+  InputGroup,
+  NavDropdown,
+  Image,
+} from 'react-bootstrap';
 
-function MainScreen(){
-   sessionStorage.clear(); 
-   return(
-      <div >
-      MainScreen
-      </div>
-   );
+function MainScreen() {
+  return (
+    <Container>
+      <Row>
+        <Col xs={6} md={12}>
+          aa
+        </Col>
+      </Row>
+      <Row>
+        <p>아메리카노</p>
+      </Row>
+    </Container>
+  );
 }
+
 export default MainScreen;
