96745d9effc052e770eab8f4da2bb2a8abb76a87
PiaoZhengxun <pjh951218@naver.com>
2021-05-31 01:51:41 +0900

    알러지 :)

--- src/Routes/MyPage/MyAllergies.js
+++ src/Routes/MyPage/MyAllergies.js
@@ -7,36 +7,69 @@ import {
 import './MyAllergies.scss';
 import axios from 'axios';
 import { setUserAllergyInfo } from 'api';
+import { Link } from 'react-router-dom';
+import {
+  Col,
+  Container,
+  Button,
+  Card,
+  CardText,
+  Row,
+  Table,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
 
 function MyAllergies() {
   const [allergy, setAllergy] = useState('');
   const [allergyList, setAllergyList] = useState([]);
 
-  console.log('0-0-0-0-0-0-0-0-0-0');
-  console.log(allergy);
+  useEffect(() => {
+    try {
+      console.log('유즈이펙트', allergy);
+      if (allergy == '') {
+        console.log('패스');
+      } else {
+        setAllergyList([...allergyList, allergy]);
+        console.log(allergyList);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, [allergy]);
 
-  const onKeyPress = (e) => {
-    if (e.key == 'Enter') {
-      onClick();
+  const onChange = (e) => {
+    if (e.target.checked == true) {
+      console.log('온체인지 true', e.target.checked);
+      setAllergy(e.target.name);
+    } else if (e.target.checked == false) {
+      console.log('온체인지 false', e.target.checked);
+      onRemove(e.target.name);
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const onCheck = () => {};
+
+  const onRemove = (name) => {
+    setAllergyList(allergyList.filter((allergy) => allergy !== name));
+    console.log('onRemove 작동', allergyList);
   };
 
-  const onChange = (e) => {
-    setAllergy(e.target.value);
-  };
+  // const oncc = useEffect(() => {
+  //   setAllergyList([...allergyList, allergy]);
+  //   console.log('여기는 함수 유즈이펙트', allergy);
+  // }, [allergy]);
 
-  const onClick = () => {
-    //여기서 서버랑 연동해서 해야함
-    console.log('여기는 클릭부분에서 나오느 콘솔콘솔코코콘솔');
-    console.log(allergy);
-    setAllergyList([...allergyList, allergy]);
-    console.log('여기서부터는 알러지 리스트 나오는 부분임');
-    console.log(allergyList);
-  };
+  // const onClick = (allergy) => {
+  //   //여기서 서버랑 연동해서 해야함
+  //   console.log('여기는 클릭부분에서 나오느 콘솔콘솔코코콘솔');
+  //   console.log('온클릭', allergy);
+  //   setAllergyList([...allergyList, allergy]);
+  //   console.log('여기서부터는 알러지 리스트 나오는 부분임');
+  //   console.log(allergyList);
+  // };
 
   const createAllergy = async () => {
     try {
@@ -48,47 +81,238 @@ function MyAllergies() {
   };
 
   return (
-    <div>
-      <p>알러지 종류</p>
+    <div className="myAllergies">
       <br />
-      <p>아몬드 우유 대두 밀 닭고기</p>
-      <p>쇠고기 새우 오징어 잣 소고기</p>
-      <p>돼지고기 메추리알 토마토 조개류 난류</p>
-      <p>호두 복숭아 땅콩 게</p>
-      <p>아황산류 메밀 계란</p>
-      <hr />
-      <p>위의 알러지 종류만 가능합니다.</p>
-      <hr />
       <br />
-      <div>
-        <form className="allergyTable" onSubmit={handleSubmit}>
-          <div>
-            <label for="inputAllergy">알러지 입력</label>
-            <input
-              type="text"
-              id="inputAllergy"
-              placeholder="알러지를 입력하세요"
-              // value={allergy}
-              onChange={onChange}
-              onKeyPress={onKeyPress}
-            />
-          </div>
-          <hr />
-          <div>{allergy}</div>
-          <hr />
-        </form>
-        <button onClick={onClick}>
-          <MdAdd />
-        </button>
-        <hr />
-        <hr />
+      <Container>
+        <div>
+          <Row>
+            <Col md="8">
+              <p className="changeAllergyInfo">알러리 정보 변경하기</p>
+            </Col>
+            <Col md="2">
+              <Button color="primary" size="sm">
+                전부해제
+              </Button>
+            </Col>
+            <Col md="2">
+              <Link to="/mypage">
+                <Button color="danger" size="sm">
+                  취소하기
+                </Button>
+              </Link>
+            </Col>
+          </Row>
+        </div>
         <br />
-        <div>{allergyList}</div>
-        <hr />
-        <hr />
-        <p>등록하는부분달거달거달거!!!! ㅎㅎ</p>
-        <button onClick={createAllergy}>등록!</button>
-      </div>
+        <br />
+        <div>
+          <Form className="checkboxGroup">
+            <Row>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="아몬드" onChange={onChange} />
+                    아몬드
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="우유" onChange={onChange} />
+                    우유
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="대두" onChange={onChange} />
+                    대두
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="밀" onChange={onChange} /> 밀
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="닭고기" onChange={onChange} />
+                    닭고기
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="쇠고기" onChange={onChange} />
+                    쇠고기
+                  </Label>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="새우" onChange={onChange} />
+                    새우
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="오징어" onChange={onChange} />
+                    오징어
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="잣" onChange={onChange} /> 잣
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="소고기" onChange={onChange} />{' '}
+                    소고기
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="돼지고기"
+                      onChange={onChange}
+                    />{' '}
+                    돼지고기
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="메추리알"
+                      onChange={onChange}
+                    />{' '}
+                    메추리알
+                  </Label>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="토마토" onChange={onChange} />{' '}
+                    토마토
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="조개류" onChange={onChange} />{' '}
+                    조개류
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="난류" onChange={onChange} />{' '}
+                    난류
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="호두" onChange={onChange} />{' '}
+                    호두
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="복숭아" onChange={onChange} />{' '}
+                    복숭아
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="땅콩" onChange={onChange} />
+                    땅콩
+                  </Label>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="게" onChange={onChange} /> 게
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="아황산류"
+                      onChange={onChange}
+                    />
+                    아황산류
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="메밀" onChange={onChange} />{' '}
+                    메밀
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col md="2">
+                <FormGroup check inline>
+                  <Label check>
+                    <Input type="checkbox" name="계란" onChange={onChange} />{' '}
+                    계란
+                  </Label>
+                </FormGroup>
+              </Col>
+            </Row>
+          </Form>
+          <Button
+            outline
+            color="warning"
+            size="sm"
+            block
+            onClick={createAllergy}
+          >
+            저장하기
+          </Button>
+        </div>
+      </Container>
     </div>
   );
 }
