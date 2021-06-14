fc7b3bf15c1967c8829cb2b6a85a6b2c5d0477da
YiGeon Park <parkegun@gmail.com>
2021-06-14 14:25:30 +0900

    fix main result

--- src/Routes/MainScreen/ResultPage.js
+++ src/Routes/MainScreen/ResultPage.js
@@ -12,7 +12,7 @@ useEffect(()=>{
   return (
     <div className='result'>
       {loading ? <div>Loading...</div> :
-        <div className='result__container'>
+        <div className='result__container' style={result && result.length === 0 ? {display:'block'} : {display:'grid'} }>
           {result && result.length > 0 ?
             result.map((result, index) => (
               <div className='card-body'>
